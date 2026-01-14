1f8f51a9a6aa29806dd6638980501de507aedb4c
sandyie715 <sanjay715@infomaticscorp.com>
2026-01-14 17:51:14 +0530

    Refactor FastAPI app and update response messages

--- api/index.py
+++ api/index.py
@@ -10,46 +10,44 @@ from bson import ObjectId
 
 load_dotenv()
 
-app = FastAPI(title= "Mongo Connection Testing")
+app = FastAPI(title="Mongo Connection Testing")
 
-USER_NAME=os.getenv("USER_NAME")
-PASSWORD=os.getenv("PASSWORD")
-
-MONGO_URL=f"mongodb+srv://{USER_NAME}:{PASSWORD}@testing.fyevyj5.mongodb.net/?appName=Testing"
-
+USER_NAME = os.getenv("USER_NAME")
+PASSWORD = os.getenv("PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
+
+MONGO_URL = f"mongodb+srv://{USER_NAME}:{PASSWORD}@testing.fyevyj5.mongodb.net/?appName=Testing"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["*"],
-    allow_methods = ["*"],
-    allow_headers = ["*"],
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# ======== DB CONNECTION ================
+# ===== DB CONNECTION =====
 client = MongoClient(MONGO_URL)
 db = client[DB_NAME]
 notes_collection = db["notes"]
 
-# ======= Models ====================
+# ===== Models =====
 class Note(BaseModel):
     title: str
     content: str
 
-
-# ------------ Helpers ------------------
+# ===== Helpers =====
 def note_serializer(note):
     return {
-        "id" : str(note["_id"]),
+        "id": str(note["_id"]),
         "title": note["title"],
         "content": note["content"],
     }
 
+# ===== Routes =====
 @app.get("/")
 def root():
-    return {"Message": "Api is Working"}
+    return {"message": "FastAPI on Vercel is working 🚀"}
 
-# CREATE (POST)
 @app.post("/notes")
 def create_note(note: Note):
     result = notes_collection.insert_one(note.dict())
@@ -67,12 +65,12 @@ def update_note(note_id: str, note: Note):
         {"$set": note.dict()}
     )
     if result.matched_count == 0:
-        raise HTTPException(status_code=404, detail= "Note not found")
-    return {"Message": "Note updated"}
+        raise HTTPException(status_code=404, detail="Note not found")
+    return {"message": "Note updated"}
 
 @app.delete("/notes/{note_id}")
 def delete_note(note_id: str):
     result = notes_collection.delete_one({"_id": ObjectId(note_id)})
     if result.deleted_count == 0:
-        raise HTTPException(status_code=404, detail= "Note not found")
-    return {"message": "Note Deleted"}
+        raise HTTPException(status_code=404, detail="Note not found")
+    return {"message": "Note deleted"}
